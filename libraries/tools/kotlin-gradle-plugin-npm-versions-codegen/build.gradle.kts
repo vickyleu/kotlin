a05ea8b471a4263d3bf54a79db77c3fe0508836b
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2023-07-12 13:00:32 +0200

    Update dependencies
    Fix some reports from "Show Vulnerable Dependencies"

--- libraries/tools/kotlin-gradle-plugin-npm-versions-codegen/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-npm-versions-codegen/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-cio:1.4.0")
+    implementation("io.ktor:ktor-client-cio:2.3.3")
     implementation(commonDependency("com.google.code.gson:gson"))
     implementation("org.apache.velocity:velocity:1.7")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0")
