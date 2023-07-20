ca0f93f134ce605056c18e9d42648ad2bfd88553
Bogdan Mukvich <bogdan.mukvich@jetbrains.com>
2023-07-20 17:47:04 +0200

    [Build] Update dependencies
    Fix some reports from "Show Vulnerable Dependencies"
    
    ^KTI-1342

--- repo/codebase-tests/build.gradle.kts
+++ repo/codebase-tests/build.gradle.kts
@@ -7,8 +7,8 @@ dependencies {
     testImplementation(intellijCore())
     testImplementation(projectTests(":compiler:tests-common"))
 
-    testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.15.2")
-    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.7")
+    testImplementation(libs.jackson.dataformat.xml)
+    testImplementation(libs.jackson.module.kotlin)
     testImplementation("com.fasterxml.woodstox:woodstox-core:6.5.1")
 
     testImplementation("org.eclipse.jgit:org.eclipse.jgit:5.13.0.202109080827-r")
