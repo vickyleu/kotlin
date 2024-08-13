302d118475be2eea0a819fc72e7ee436b17a1613
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2024-08-13 09:41:10 +0200

    ~ added test from last discussion

--- compiler/testData/codegen/box/multiplatform/k2/kmpCpPrototype/classes1.kt
+++ compiler/testData/codegen/box/multiplatform/k2/kmpCpPrototype/classes1.kt
@@ -1,36 +1,27 @@
 // FIR_IDENTICAL
 // LANGUAGE: +MultiPlatformProjects
 // TARGET_BACKEND: JVM
-// WITH_STDLIB
 
 // MODULE: lib-common
 // FILE: libCommon.kt
+class Bar()
 
-class Bar(val v: String = "common")
-
-fun foo(a: String = "") = "common" // 1 klib
-//fun foo(a: String = "") = "OK" // 2 lib-jvm.jar
+fun foo(a: String = "") = "OK"
 
 // MODULE: lib-jvm()()(lib-common)
 // FILE: libPlatform.kt
-
-fun foo() = "platform"
 
 // MODULE: app-common(lib-common)()()
 // FILE: appCommon.kt
 expect fun bbb(): Bar
 
-fun commonBox() = Bar()
+fun commonBox(): Bar = Bar()
 
 // MODULE: app-jvm(lib-jvm)()(app-common)
 // FILE: app.kt
 actual fun bbb(): Bar = commonBox()
 
 fun box(): String {
-    val commonBox = commonBox().v
-    val commonDirect = foo("")
-    val platformDirect = foo()
-    return if (commonBox == "common" && commonDirect == "common" && platformDirect == "platform") "OK"
-    else "$commonBox/$commonDirect/$platformDirect"
+    commonBox()
+    return "OK"
 }
-
