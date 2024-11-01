17eefc438c443aa80374c10f0fe631f37be12f62
Igor Chevdar <igor.chevdar@jetbrains.com>
2024-11-01 21:13:13 +0200

    fixup! FileCheck tests

--- native/native.tests/testData/codegen/fileCheck/remove_redundant_type_checks.kt
+++ native/native.tests/testData/codegen/fileCheck/remove_redundant_type_checks.kt
@@ -41,10 +41,66 @@ fun test3(o: Any): Int {
 // CHECK-LABEL: epilogue:
 }
 
+// CHECK-LABEL: define i32 @"kfun:#test4(kotlin.Int;kotlin.Any){}kotlin.Int
+fun test4(x: Int, o: Any): Int {
+// CHECK-DEBUG: {{call|call zeroext}} i1 @IsSubtype
+// CHECK-OPT: {{call|call zeroext}} i1 @IsSubclassFast
+    val result = if (x == 42 || o is String)
+// CHECK-DEBUG: {{call|call zeroext}} i1 @IsSubtype
+// CHECK-OPT: {{call|call zeroext}} i1 @IsSubclassFast
+// CHECK: call i32 @"kfun:kotlin.String#<get-length>(){}kotlin.Int
+        (o as? String)?.length ?: 0
+    else
+        x
+    return result
+// CHECK-LABEL: epilogue:
+}
+
+// CHECK-LABEL: define {{i1|zeroext i1}} @"kfun:#baz(kotlin.String){}kotlin.Boolean"
+fun baz(s: String) = s.length == 3
+// CHECK-LABEL: epilogue:
+
+// CHECK-LABEL: define i32 @"kfun:#test5(kotlin.Int;kotlin.Any){}kotlin.Int
+fun test5(x: Int, o: Any): Int {
+// CHECK-DEBUG: {{call|call zeroext}} i1 @IsSubtype
+// CHECK-OPT: {{call|call zeroext}} i1 @IsSubclassFast
+    val result = if (x == 42 || baz(o as String))
+// CHECK-DEBUG: {{call|call zeroext}} i1 @IsSubtype
+// CHECK-OPT: {{call|call zeroext}} i1 @IsSubclassFast
+// CHECK: call i32 @"kfun:kotlin.String#<get-length>(){}kotlin.Int
+        (o as? String)?.length ?: 0
+    else
+// CHECK-DEBUG-NOT: {{call|call zeroext}} i1 @IsSubtype
+// CHECK-OPT-NOT: {{call|call zeroext}} i1 @IsSubclassFast
+// CHECK: {{call|call zeroext}} i16 @Kotlin_String_get
+        (o as? String)?.get(0)?.code ?: x
+    return result
+// CHECK-LABEL: epilogue:
+}
+
+// CHECK-LABEL: define i32 @"kfun:#test6(kotlin.Int;kotlin.Any){}kotlin.Int
+fun test6(x: Int, o: Any): Int {
+// CHECK-DEBUG: {{call|call zeroext}} i1 @IsSubtype
+// CHECK-OPT: {{call|call zeroext}} i1 @IsSubclassFast
+    if (x == 42 || baz(o as String))
+// CHECK-DEBUG: {{call|call zeroext}} i1 @IsSubtype
+// CHECK-OPT: {{call|call zeroext}} i1 @IsSubclassFast
+// CHECK: call i32 @"kfun:kotlin.String#<get-length>(){}kotlin.Int
+        return (o as? String)?.length ?: 0
+// CHECK-DEBUG-NOT: {{call|call zeroext}} i1 @IsSubtype
+// CHECK-OPT-NOT: {{call|call zeroext}} i1 @IsSubclassFast
+// CHECK: {{call|call zeroext}} i16 @Kotlin_String_get
+    return (o as? String)?.get(0)?.code ?: x
+// CHECK-LABEL: epilogue:
+}
+
 // CHECK-LABEL: define ptr @"kfun:#box(){}kotlin.String"
 fun box(): String {
     println(test1("zzz"))
     println(test2("zzz"))
     println(test3("zzz"))
+    println(test4(1, "zzz"))
+    println(test5(1, "zzz"))
+    println(test6(1, "zzz"))
     return "OK"
 }
