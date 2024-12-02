8a8abe3a28972058da783626d7b2f1408324d122
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-12-02 16:24:31 +0100

    WIP! 2024-12-02T15:24:30Z wip

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/ResolveUtils.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/ResolveUtils.kt
@@ -377,6 +377,8 @@ fun BodyResolveComponents.buildResolvedQualifierForClass(
         } else {
             setTypeOfQualifier(this@buildResolvedQualifierForClass)
         }
+    }.also {
+        // println("${it::class} ${it.classId} ${it.symbol} relativeClassName=${relativeClassName}")
     }
 }
 
