c170a4ecf510f939fe129e6e5ef8d2bf3d41c29d
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-01-08 17:54:29 +0100

    ~ test data

--- compiler/testData/diagnostics/tests/functionTypeInitializerTypeMismatch.fir.kt
+++ compiler/testData/diagnostics/tests/functionTypeInitializerTypeMismatch.fir.kt
@@ -9,14 +9,14 @@ val f3: () -> Int = l@ {
     if (true) return@l 4
     return@l <!RETURN_TYPE_MISMATCH("kotlin.Int; kotlin.String")!>""<!>
 }
-val f5: () -> Int = <!INITIALIZER_TYPE_MISMATCH("kotlin.Function0<kotlin.Int>; kotlin.Function1<ERROR CLASS: Cannot infer type for parameter it, kotlin.String>")!>{ <!CANNOT_INFER_PARAMETER_TYPE!>it<!> -> "" }<!>
-val f6: () -> Int = <!INITIALIZER_TYPE_MISMATCH("kotlin.Function0<kotlin.Int>; kotlin.Function1<kotlin.Any, kotlin.String>")!>{ it: Any -> "" }<!>
+val f5: () -> Int = <!ARGUMENT_TYPE_MISMATCH("kotlin.Function0<kotlin.Int>; kotlin.Function1<ERROR CLASS: Cannot infer type for parameter it, kotlin.Int>"), INITIALIZER_TYPE_MISMATCH("kotlin.Function0<kotlin.Int>; kotlin.Function1<ERROR CLASS: Cannot infer type for parameter it, kotlin.Int>")!>{ <!CANNOT_INFER_PARAMETER_TYPE!>it<!> -> <!RETURN_TYPE_MISMATCH("kotlin.Int; kotlin.String")!>""<!> }<!>
+val f6: () -> Int = <!ARGUMENT_TYPE_MISMATCH("kotlin.Function0<kotlin.Int>; kotlin.Function1<kotlin.Any, kotlin.Int>"), INITIALIZER_TYPE_MISMATCH("kotlin.Function0<kotlin.Int>; kotlin.Function1<kotlin.Any, kotlin.Int>")!>{ it: Any -> <!RETURN_TYPE_MISMATCH("kotlin.Int; kotlin.String")!>""<!> }<!>
 val f7: () -> Int = {  -> <!RETURN_TYPE_MISMATCH("kotlin.Int; kotlin.String")!>""<!> }
 val f8: String.() -> Int = {  -> <!RETURN_TYPE_MISMATCH("kotlin.Int; kotlin.String")!>""<!> }
-val f9: String = <!INITIALIZER_TYPE_MISMATCH("kotlin.String; kotlin.Function0<kotlin.String>")!>{  -> "" }<!>
+val f9: String = <!ARGUMENT_TYPE_MISMATCH("kotlin.String; kotlin.Function0<kotlin.String>"), INITIALIZER_TYPE_MISMATCH("kotlin.String; kotlin.Function0<kotlin.String>")!>{  -> "" }<!>
 val f10: Function0<Int> = {  -> <!RETURN_TYPE_MISMATCH("kotlin.Int; kotlin.String")!>""<!> }
 val f11: Function0<<!REDUNDANT_PROJECTION("kotlin.Function0<out kotlin.Int>")!>out<!> Int> = {  -> <!RETURN_TYPE_MISMATCH("kotlin.Int; kotlin.String")!>""<!> }
-val f12: Function0<<!CONFLICTING_PROJECTION("kotlin.Function0<in kotlin.Int>")!>in<!> Int> = {  -> "" }
+val f12: Function0<<!CONFLICTING_PROJECTION("kotlin.Function0<in kotlin.Int>")!>in<!> Int> = {  -> <!RETURN_TYPE_MISMATCH("kotlin.Int; kotlin.String")!>""<!> }
 val f13: Function0<*> = {  -> "" }
 val f14: Function<Int> = {  -> <!RETURN_TYPE_MISMATCH("kotlin.Int; kotlin.String")!>""<!> }
 val f15: Function<Int> = { <!RETURN_TYPE_MISMATCH("kotlin.Int; kotlin.String")!>""<!> }
