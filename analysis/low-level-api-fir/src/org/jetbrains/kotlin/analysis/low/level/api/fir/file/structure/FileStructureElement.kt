878188b65efd99b79bb9b8f8797a6cd34a1c33d7
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2025-01-31 23:06:00 +0900

    fixup! [Analysis API] Implement partial body analysis
    Invalidate partial body element providers on in-block modifications.

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/file/structure/FileStructureElement.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/file/structure/FileStructureElement.kt
@@ -253,6 +253,11 @@ internal class DeclarationStructureElement(
                     val psiStatements = psiBodyBlock?.statements
 
                     if (isPartiallyResolvable && session != null && psiStatements != null && psiStatements.size > 1) {
+                        // Although we don't require the body to be resolved here, its changes must invalidate the element provider.
+                        // Note that there might be changes in a number of statements, so here we keep the guarantee – a partial provider
+                        // is only created if there are more than one body statement.
+                        LLFirDeclarationModificationService.bodyResolved(declaration, phase = FirResolvePhase.BODY_RESOLVE)
+
                         return PartialBodyDeclarationFirElementProvider(declaration, psiDeclaration, psiBodyBlock, psiStatements, session)
                     }
                 }
