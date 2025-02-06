9361adcf4b079dd534c17058fee84225658988fc
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-02-06 14:50:31 +0100

    fixup! FE: Resolve top-level lambdas via the same rules as for call arguments

---
+++ compiler/fir/analysis-tests/testData/resolve/inference/samConversionTopLevel.latestLV.kt
@@ -0,0 +1,30 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// SKIP_FIR_DUMP
+// ISSUE: KT-67869
+// LATEST_LV_DIFFERENCE
+
+fun interface MyFun {
+    fun foo(x: String): Int
+}
+
+val topLevel: MyFun = { it.length }
+
+fun baz(x: MyFun = { it.length }): MyFun = x
+
+class A(
+    val classMember: MyFun = { it.length }
+)
+
+fun returnExpr(): MyFun = { it.length }
+
+fun returnExplicit(): MyFun {
+    return { it.length }
+}
+
+val withGetter: MyFun
+    get() = { it.length }
+
+fun main() {
+    var local: MyFun = { it.length }
+    local = { it.length + 1 }
+}
