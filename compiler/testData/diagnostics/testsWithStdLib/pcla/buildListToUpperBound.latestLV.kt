fb9dd3b3ed369f0396c9340763cf08a170cf8113
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-02-07 16:26:35 +0100

    ~~~~

---
+++ compiler/testData/diagnostics/testsWithStdLib/pcla/buildListToUpperBound.latestLV.kt
@@ -0,0 +1,13 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// LATEST_LV_DIFFERENCE
+// WITH_STDLIB
+// LANGUAGE: -ForbidInferringPostponedTypeVariableIntoDeclaredUpperBound
+// ISSUE: KT-50520
+
+fun box(): String {
+    <!CANNOT_INFER_PARAMETER_TYPE, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>buildList<!> {
+        val foo = <!CANNOT_INFER_PARAMETER_TYPE!>{ <!CANNOT_INFER_PARAMETER_TYPE, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>first<!>() }<!>
+        add(0, foo)
+    }
+    return "OK"
+}
