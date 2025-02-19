6b4277203fdba015adac69ed0a7d6b262dee04b8
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-02-19 23:52:13 +0100

    [Native][Klib][Tests] Implement backward klib compatibility testing with v2.1
    ^KT-71957

--- compiler/testData/codegen/box/annotations/instances/rearrangedArgumentsInAnnotationCall.kt
+++ compiler/testData/codegen/box/annotations/instances/rearrangedArgumentsInAnnotationCall.kt
@@ -1,4 +1,6 @@
 // ISSUE: KT-73845
+// IGNORE_NATIVE: compatibilityTestMode=BACKWARD_2_1
+// ^^^ KT-73845: Compiler v2.1.0 has a bug in 1st compilation phase
 
 annotation class A
 annotation class B(
