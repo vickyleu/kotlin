0841248419e8d2048aca16c60312b50603d09f20
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-02-21 17:55:29 +0100

    ~ property access always named

--- compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/FirTree.kt
+++ compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/FirTree.kt
@@ -892,6 +892,7 @@ object FirTree : AbstractFirTreeBuilder() {
 
     val propertyAccessExpression: Element by element(Expression) {
         parent(qualifiedAccessExpression)
+        +field("calleeReference", namedReference, withReplace = true, withTransform = true)
     }
 
     val getClassCall: Element by element(Expression) {
