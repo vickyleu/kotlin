0f3f48ca6d3084318763cfd51ed5fc902770ebe4
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2025-03-02 16:53:46 +0100

    Fir: Support @JvmExposeBoxed annotation
     #KT-73466

--- compiler/fir/checkers/checkers.jvm/src/org/jetbrains/kotlin/fir/analysis/jvm/checkers/declaration/FirJvmFieldApplicabilityChecker.kt
+++ compiler/fir/checkers/checkers.jvm/src/org/jetbrains/kotlin/fir/analysis/jvm/checkers/declaration/FirJvmFieldApplicabilityChecker.kt
@@ -99,15 +99,6 @@ object FirJvmFieldApplicabilityChecker : FirPropertyChecker(MppCheckerKind.Commo
         reporter.reportOn(annotation.source, factory, problem.errorMessage, context)
     }
 
-    private fun FirTypeRef.isInlineClassThatRequiresMangling(session: FirSession): Boolean {
-        val symbol = this.coneType.toRegularClassSymbol(session) ?: return false
-        return symbol.isInlineOrValue && !symbol.isDontMangleClass()
-    }
-
-    private fun FirRegularClassSymbol.isDontMangleClass(): Boolean {
-        return this.classId == StandardClassIds.Result
-    }
-
     private fun FirProperty.isOverridable(containingClass: FirRegularClassSymbol?): Boolean {
         return visibility != Visibilities.Private && modality != Modality.FINAL &&
                 containingClass?.isFinal != true
@@ -152,4 +143,13 @@ object FirJvmFieldApplicabilityChecker : FirPropertyChecker(MppCheckerKind.Commo
             it.annotationTypeRef.coneType.classId == JVM_MULTIFILE_CLASS_ID
         } == true
     }
+}
+
+fun FirTypeRef.isInlineClassThatRequiresMangling(session: FirSession): Boolean {
+    val symbol = this.coneType.toRegularClassSymbol(session) ?: return false
+    return symbol.isInlineOrValue && !symbol.isDontMangleClass()
+}
+
+private fun FirRegularClassSymbol.isDontMangleClass(): Boolean {
+    return this.classId == StandardClassIds.Result
 }
